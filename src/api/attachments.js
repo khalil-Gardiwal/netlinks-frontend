b469cf1644bc13d283c0921349a1a874a68714d5
Mohammad Khalil Gardiwal <mgardiwal@netlinks.af>
2026-08-23 14:07:08 +0430

    added new translations

--- src/api/attachments.js
+++ src/api/attachments.js
@@ -8,18 +8,4 @@ export const uploadAttachment = (file) => {
   return api.post("/attachments", formData);
 };
 
-export const getAttachment = (id) => {
-  return api.get(`/attachments/${id}`);
-};
 
-export const claimAttachment = (id) => {
-  return api.post(`/attachments/${id}/claim`);
-};
-
-export const deleteAttachment = (id) => {
-  return api.delete(`/attachments/${id}`);
-};
-
-export const getAttachmentFileUrl = (id) => {
-  return `${api.defaults.baseURL}/attachments/${id}/file`;
-};
